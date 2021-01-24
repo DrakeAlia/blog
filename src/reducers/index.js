bf8638c962b173845fcc94a8183432492fb14926
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2021-01-24 15:43:24 -0800

    return values from reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import postsReducer from './postsReucer.js';
+import postsReducer from './postsReducer.js';
 
 export default combineReducers({
     posts: postsReducer
