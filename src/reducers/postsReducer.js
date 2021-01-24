cbe6e454f4174a0735ae3482b426917670166695
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2021-01-24 15:55:19 -0800

    pure reducers

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,3 +1,12 @@
 export default () => {
     return null;
+
+    // bad!:
+    // return document.querySelector('input');
+
+    // bad: 
+    // return axios.get('/posts);
+
+    // good:
+    // return state + action
 };
