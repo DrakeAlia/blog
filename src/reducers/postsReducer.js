a7d5d92903230dd55ff812adbadbe34d7889cc84
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2021-01-24 17:38:56 -0800

    dispatching correct values

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -24,3 +24,28 @@ export default (state = [], action) => {
           return state;
     }
 };
+
+
+// =============================================================================
+// Coding Exercise 13: Adding a Reducer Case:
+// export default (state = [], action) => {
+//     switch (action.type) {
+//         case 'FETCH_POSTS':
+//             return action.payload;
+//         default:
+//             return state;
+//     }
+// }
+
+
+// Solution:
+// export default (state = [], action) => {
+//     switch (action.type) {
+//         case 'FETCH_POSTS':
+//             return action.payload;
+//          case 'ADD_POST':
+//              return [...state, payload.action]
+//         default:
+//             return state;
+//     }
+// }
