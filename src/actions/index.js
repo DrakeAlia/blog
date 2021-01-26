78318ca272c8ca23f10cec16347ddde311c7b161
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2021-01-25 19:50:49 -0800

    action creators in action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,11 @@
 import _ from 'lodash';
-import jsonPlaceholder from '../apis/jsonPlaceholder'
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPostsAndUsers = () => async dispatch => {
+   console.log('About to fetch posts');
+   await dispatch(fetchPosts());
+   console.log('fetched posts!');
+};
 
 export const fetchPosts = () => async (dispatch) => {
     const response = await jsonPlaceholder.get('/posts');
@@ -8,11 +14,17 @@ export const fetchPosts = () => async (dispatch) => {
 };
 // ^^^^^^^^^ You will see this in alot of redux projects ^^^^^^^^^
    
-export const fetchUser = (id) => dispatch => {
-    _fetchUser(id, dispatch);
-};
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = id => async dispatch => {
     const response = await jsonPlaceholder.get(`/users/${id}`);
 
     dispatch({ type: 'FETCH_USER', payload: response.data });
-});
+};
+
+
+// Memoize Version:
+// export const fetchUser = (id) => dispatch => _fetchUser(id, dispatch);
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//     dispatch({ type: 'FETCH_USER', payload: response.data });
+// });
